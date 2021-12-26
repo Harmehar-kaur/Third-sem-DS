3be790404bc9b7c9fec4de67e3876a150b4aa78f
Harmehar-kaur <84430398+Harmehar-kaur@users.noreply.github.com>
2021-12-26 10:03:33 +0530

    Update shellsort.c

--- shellsort.c
+++ shellsort.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+void shellsort(int a[], int n)
+{
+    int gap = n, i, temp, flag = 1;
+    while (flag == 1 || gap > 1)
+    {
+        flag = 0;
+        gap = (gap + 1) / 2;
+        for (i = 0; i < (n - gap); i++)
+        {
+            if (a[i + gap] < a[i])
+            {
+                temp = a[i + gap];
+                a[i + gap] = a[i];
+                a[i] = temp;
+                flag = 0;
+            }
+        }
+    }
+}
 int main()
 {
     system("cls");
-    int n;
-    printf("\n Enter number of elements: ");
-    scanf("%d", &n);
-    int a[n];
-    printf("\n Enter elements: \n ");
-    for (int i = 0; i < n; i++)
+    int m, i;
+    printf("\n Enter no. of elements: ");
+    scanf("%d", &m);
+    int x[m];
+    printf("\n Enter elements: \n");
+    for (i = 0; i < m; i++)
     {
-        scanf("%d", &a[i]);
+        scanf("%d", &x[i]);
     }
-
-    for (int i = 0; i < n - 1; i++)
-    {
-
-        for (int j = i + 1; j < n; j++)
-        {
-            if (a[i] > a[j])
-            {
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
-        }
-
-        printf("\n %d", i);
-        printf(".");
-        for (int x = 0; x < n; x++)
-        {
-            printf(" %d ", a[x]);
-        }
-    }
-
-    printf("\n Harmehar Kaur (CSE-1 04613202720)\n");
+    shellsort(x, m);
+    printf("\n Sorted array is : \n");
+    for (i = 0; i < m; i++)
+        printf(" %d ", x[i]);
+    printf("\n Harmehar Kaur (CSE-1 04613202720)Shell Sort\n");
 }
